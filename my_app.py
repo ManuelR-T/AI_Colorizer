b8946ba32e5ae8b712b388f04cbccd6f3a428dde
adrien.ckn <adrien.cokini@epitech.eu>
2023-02-11 13:19:52 +0100

    [ADD] train loaders

--- my_app.py
+++ my_app.py
@@ -8,23 +8,39 @@ EPOCHS = 10
 BATCH_SIZE = 64
 LR = 0.001
 NB_OUTPUTS = 10
+PATH = './datasets'
 
-class ai_colorizer():
+class AIColorizer():
     def __init__(self):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = model(epochs=EPOCHS, batch_size=BATCH_SIZE, l_r=LR, nb_outputs=NB_OUTPUTS)
         self.model.to(self.device)
-        self.train_loader = self.get_data("./datasets/")
-        self.test_loader = self.get_data("./datasets/", train=False)
+        self.train_loader_g =   self.get_data("./datasets/") # Train grayscale
+        self.test_loader_g  =   self.get_data("./datasets/", train=False) # Test grayscale
+        self.train_loader_c =   self.get_data("./datasets/", grayscale=False) # Train colored
+        self.test_loader_c  =   self.get_data("./datasets/", grayscale=False, train=False) # Test colored
 
-    def get_data(self, path, train: bool = True):
-        transform = transforms.Compose(
-                [
-                    transforms.Grayscale(num_output_channels = 1),
-                    transforms.ToTensor(),
-                    transforms.Normalize((0.5,), (0.5,))
-                ]
-            )
+    def get_data(
+            self,
+            path,
+            grayscale: bool = True,
+            train: bool = True
+            ):
+        if grayscale:
+            transform = transforms.Compose(
+                    [
+                        transforms.Grayscale(num_output_channels = 1),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5,), (0.5,))
+                    ]
+                )
+        else:
+            transform = transforms.Compose(
+                    [
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5,), (0.5,))
+                    ]
+                )
         dataset = torchvision.datasets.CIFAR100(
             root=path,
             train=train,
@@ -45,4 +61,4 @@ class ai_colorizer():
     def model_load(self, model_path):
         self.model.load_state_dict(torch.load(model_path))
 
-
+ai = AIColorizer()
